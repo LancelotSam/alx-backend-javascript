6d597784e27c89ab6d14dd3708113a03b15099d7
LancelotSam <skmburu110@gmail.com>
2024-07-15 01:30:37 -0400

    modifed for lint

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,5 +1,10 @@
 export const weakMap = new WeakMap();
 
+/**
+ * The maximum number of calls for an endpoint.
+ */
+const MAX_ENDPOINT_CALLS = 5;
+
 export const queryAPI = (endpoint) => {
   if (weakMap.has(endpoint)) {
     const endpointData = weakMap.get(endpoint);
