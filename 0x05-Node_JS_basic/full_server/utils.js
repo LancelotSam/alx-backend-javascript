6565873e19f03b50dfddbb71f85e14c08b0e9fc3
LancelotSam <skmburu110@gmail.com>
2024-07-27 00:34:52 +0000

    babel configs errors

--- 0x05-Node_JS_basic/full_server/utils.js
+++ 0x05-Node_JS_basic/full_server/utils.js
@@ -1,44 +1,34 @@
-import fs from 'fs';
+const fs = require('fs');
 
-/**
- * Reads the data of students in a CSV data file.
- */
 const readDatabase = (dataPath) => new Promise((resolve, reject) => {
   if (!dataPath) {
     reject(new Error('Cannot load the database'));
+    return;
   }
-  if (dataPath) {
-    fs.readFile(dataPath, (err, data) => {
-      if (err) {
-        reject(new Error('Cannot load the database'));
-      }
-      if (data) {
-        const fileLines = data
-          .toString('utf-8')
-          .trim()
-          .split('\n');
-        const studentGroups = {};
-        const dbFieldNames = fileLines[0].split(',');
-        const studentPropNames = dbFieldNames
-          .slice(0, dbFieldNames.length - 1);
 
-        for (const line of fileLines.slice(1)) {
-          const studentRecord = line.split(',');
-          const studentPropValues = studentRecord
-            .slice(0, studentRecord.length - 1);
-          const field = studentRecord[studentRecord.length - 1];
-          if (!Object.keys(studentGroups).includes(field)) {
-            studentGroups[field] = [];
-          }
-          const studentEntries = studentPropNames
-            .map((propName, idx) => [propName, studentPropValues[idx]]);
-          studentGroups[field].push(Object.fromEntries(studentEntries));
-        }
-        resolve(studentGroups);
+  fs.readFile(dataPath, 'utf-8', (err, data) => {
+    if (err) {
+      reject(new Error('Cannot load the database'));
+      return;
+    }
+
+    const fileLines = data.trim().split('\n');
+    const studentGroups = {};
+
+    for (const line of fileLines.slice(1)) {
+      const studentRecord = line.split(',');
+      const firstName = studentRecord[0];
+      const field = studentRecord[studentRecord.length - 1];
+
+      if (!studentGroups[field]) {
+        studentGroups[field] = [];
       }
-    });
-  }
+
+      studentGroups[field].push(firstName);
+    }
+
+    resolve(studentGroups);
+  });
 });
 
-export default readDatabase;
 module.exports = readDatabase;
